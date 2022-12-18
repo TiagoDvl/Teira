04dda5eb9ce741f101a1a712de293177b7dd7189
tiagodvl <tdavilas@gmail.com>
2022-12-18 00:22:11 +0000

    [23] - Code remodel of Configuration

--- ui/src/main/java/br/com/tick/ui/screens/configuration/viewmodels/SettingsScreenViewModel.kt
+++ ui/src/main/java/br/com/tick/ui/screens/configuration/viewmodels/SettingsScreenViewModel.kt
@@ -3,23 +3,30 @@ package br.com.tick.ui.screens.configuration.viewmodels
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import br.com.tick.sdk.repositories.LocalDataRepository
+import br.com.tick.ui.di.DefaultDispatcherProvider
+import br.com.tick.ui.di.DispatcherProvider
 import br.com.tick.ui.screens.configuration.states.MonthlyIncomeStates
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class ConfigurationScreenViewModel @Inject constructor(
-    private val dataStoreRepository: LocalDataRepository
+class SettingsScreenViewModel @Inject constructor(
+    private val dataStoreRepository: LocalDataRepository,
+    private val dispatcherProvider: DispatcherProvider
 ) : ViewModel() {
 
-    val monthlyIncomeFlow = dataStoreRepository.getMonthlyIncome().map {
-        MonthlyIncomeStates.of(it.monthlyIncomeValue)
-    }
+    val monthlyIncomeFlow = dataStoreRepository
+        .getMonthlyIncome()
+        .flowOn(dispatcherProvider.io())
+        .map {
+            MonthlyIncomeStates.of(it.monthlyIncomeValue)
+        }
 
     fun saveMonthlyIncome(value: Double) {
-        viewModelScope.launch {
+        viewModelScope.launch(dispatcherProvider.io()) {
             dataStoreRepository.saveMonthlyIncome(value)
         }
     }
