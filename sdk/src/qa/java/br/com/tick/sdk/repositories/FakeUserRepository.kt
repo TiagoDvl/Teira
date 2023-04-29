4de2ccab8a5d2fdbbca0ad0ea5277ce7487d0682
Tiago Dávila <tdavilas@gmail.com>
2023-04-29 20:02:56 +0100

    [50] Fixing Tests

---
+++ sdk/src/qa/java/br/com/tick/sdk/repositories/FakeUserRepository.kt
@@ -0,0 +1,36 @@
+package br.com.tick.sdk.repositories
+
+import br.com.tick.sdk.database.entities.User
+import br.com.tick.sdk.domain.AccountingDate
+import br.com.tick.sdk.domain.CurrencyFormat
+import br.com.tick.sdk.domain.NotificationPeriodicity
+import br.com.tick.sdk.repositories.user.UserRepository
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.first
+
+class FakeUserRepository: UserRepository {
+
+    private val user: MutableSharedFlow<User> = MutableSharedFlow(replay = 1)
+
+    init {
+        user.tryEmit(User(0, 0.0, NotificationPeriodicity.DAILY, CurrencyFormat.REAL, AccountingDate.DAY_ONE))
+    }
+
+    override fun getUser() = user
+
+    override suspend fun setMonthlyIncome(newValue: Double) {
+        user.emit(getUser().first().copy(monthlyIncome = newValue))
+    }
+
+    override suspend fun setNotificationPeriodicity(notificationPeriodicity: NotificationPeriodicity) {
+        user.tryEmit(getUser().first().copy(notificationPeriodicity = notificationPeriodicity))
+    }
+
+    override suspend fun setCurrency(currencyFormat: CurrencyFormat) {
+        user.tryEmit(getUser().first().copy(currency = currencyFormat))
+    }
+
+    override suspend fun setAccountingDate(accountingDate: AccountingDate) {
+        user.tryEmit(getUser().first().copy(accountingDate = accountingDate))
+    }
+}
