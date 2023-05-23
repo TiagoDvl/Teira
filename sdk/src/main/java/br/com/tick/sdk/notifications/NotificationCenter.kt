e7c6c6ef47d6fe3063a23c309bcb04cd66fe471e
Tiago Dávila <tdavilas@gmail.com>
2023-05-23 23:23:07 +0100

    [58] Adding action to notification click

--- sdk/src/main/java/br/com/tick/sdk/notifications/NotificationCenter.kt
+++ sdk/src/main/java/br/com/tick/sdk/notifications/NotificationCenter.kt
@@ -1,6 +1,14 @@
 package br.com.tick.sdk.notifications
 
+import android.app.PendingIntent
+
 interface NotificationCenter {
 
-    suspend fun sendNotification(title: String, text: String, icon: Int, channelId: String)
+    suspend fun sendNotification(
+        title: String,
+        text: String,
+        icon: Int,
+        channelId: String,
+        pendingIntent: PendingIntent
+    )
 }
